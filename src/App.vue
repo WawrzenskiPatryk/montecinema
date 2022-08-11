2549b04c672eba03e49980a4cebcb6cb4209c8a9
Patryk Wawrzenski <wawrzenskipatryk@gmail.com>
2022-08-11 12:18:00 +0200

    sass, normalize.css and vite-svg-loader setup

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import DeleteIcon from './assets/icons/delete-2 1.svg';
+
+export default {
+  components: {
+    DeleteIcon,
+  },
+};
+</script>
+
+<template>
+  <p>Test text</p>
+  <DeleteIcon class="icon" />
+</template>
+
+<style lang="scss">
+p {
+  & {
+    color: red;
+  }
+}
+</style>
