1192269c7f09b8ceb2037bba3c0de90e2fcf95d4
Patryk Wawrzeński <patryk.wawrzenski@monterail.com>
2022-08-16 18:58:38 +0200

    app content positioning

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,34 @@ export default defineComponent({
 
 <template>
   <div class="app">
-    <TheHeader></TheHeader>
-    <main></main>
+    <TheHeader class="app__header"></TheHeader>
+    <main class="app__content">
+      <div class="app__content-element"></div>
+    </main>
   </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.app {
+  // TODO: max-width to be considered if is it wanted or not
+  // max-width: 144rem;
+  margin: 0 auto;
+
+  &__header,
+  &__content {
+    @include screen-medium {
+      padding: 0 4.8rem;
+    }
+  }
+
+  &__content {
+    height: 100vh;
+    background-color: aquamarine;
+  }
+
+  &__content-element {
+    height: 100%;
+    background-color: cadetblue;
+  }
+}
+</style>
