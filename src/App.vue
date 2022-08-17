d23bdec68ae689360d70247e7ab7803b8b349235
Patryk Wawrzeński <patryk.wawrzenski@monterail.com>
2022-08-17 15:00:38 +0200

    HeroBanner component for HomePage view

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script>
 import { defineComponent } from 'vue';
 import TheHeader from '@/components/TheHeader.vue';
+import HomePage from '@/pages/HomePage.vue';
 
 export default defineComponent({
   components: {
     TheHeader,
+    HomePage,
   },
 });
 </script>
@@ -12,20 +14,18 @@ export default defineComponent({
 <template>
   <div class="app">
     <TheHeader class="app__header"></TheHeader>
-    <main class="app__content"></main>
+    <!-- will be <RouterView /> after implementing routing -->
+    <HomePage class="app__content" />
+    <!---->
   </div>
 </template>
 
 <style lang="scss">
 .app {
-  // TODO: max-width to be considered if is it wanted or not
-  // max-width: 144rem;
-  margin: 0 auto;
-
   &__header,
   &__content {
-    @include screen-medium {
-      padding: 0 4.8rem;
+    @include screen-min-medium {
+      padding: $page-padding-regular;
     }
   }
 }
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,16 @@
+<script>
+import { defineComponent } from 'vue';
+import HeroBanner from '@/components/home/HeroBanner.vue';
+
+export default defineComponent({
+  components: { HeroBanner },
+});
+</script>
+
+<template>
+  <main class="homepage">
+    <HeroBanner class="homepage__hero-banner" />
+  </main>
+</template>
+
+<style lang="scss" scoped></style>
