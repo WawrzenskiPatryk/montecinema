0fc580b8954c6cef9e24112295d3320bcbb4b523
Patryk Wawrzeński <patryk.wawrzenski@monterail.com>
2022-08-22 10:24:12 +0200

    api service response based on endpoint string data

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,25 @@ import axios from 'axios';
 
 const api = import.meta.env.VITE_API;
 
+const endpoint = {
+  USERS: 'users',
+  TICKET_TYPES: 'ticket_types',
+  RESERVATION_STATUSES: 'reservation_statuses',
+  SEANCES: 'seances',
+  MOVIES: 'movies',
+  TICKET_DESKS: 'ticket_desks',
+  HALLS: 'halls',
+  MOVIE_GENRES: 'movie_genres',
+  TICKETS: 'tickets',
+  RESERVATIONS: 'reservations',
+};
+
+async function getDataFromApi(endpoint) {
+  const response = await axios.get(`${api}/${endpoint}`);
+  return response;
+}
+
 export async function getAllMoviesData() {
-  const response = await axios.get(`${api}/movies`);
-  return response.data;
+  const moviesResponse = await getDataFromApi(endpoint.MOVIES);
+  return moviesResponse.data;
 }
