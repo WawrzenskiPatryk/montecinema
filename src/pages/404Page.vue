7a90fe27d80e93c4de7dfa16e1e905f283db4032
Patryk Wawrzeński <patryk.wawrzenski@monterail.com>
2022-08-22 17:29:21 +0200

    404 page update

--- src/pages/404Page.vue
+++ src/pages/404Page.vue
@@ -1,34 +1,4 @@
-<script>
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-  data() {
-    return {
-      countdown: 10,
-      timeout: null,
-      interval: null,
-    };
-  },
-  mounted() {
-    this.timeout = setTimeout(() => {
-      this.$router.replace({ name: 'HomePage' });
-    }, 10000);
-
-    this.interval = setInterval(() => {
-      this.countdown--;
-    }, 1000);
-  },
-  unmounted() {
-    clearTimeout(this.timeout);
-    clearInterval(this.interval);
-    this.timeout = null;
-    this.interval = null;
-  },
-});
-</script>
-
 <template>
   <h1>Sorry something went wrong...</h1>
-  <h2>You will be redirected to Home Page in: {{ countdown }}</h2>
   <router-link :to="{ name: 'HomePage' }" replace>Go to home page</router-link>
 </template>
