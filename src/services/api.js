f03d96db6b0585fcb85e7b74cffcfbfa4b7718ac
Patryk Wawrzeński <patryk.wawrzenski@monterail.com>
2022-09-07 10:19:13 +0200

    api.js refactor with default axios instance

--- src/services/api.js
+++ src/services/api.js
@@ -2,29 +2,30 @@ import axios from 'axios';
 
 const api = import.meta.env.VITE_API;
 
-const endpoint = {
+export const endpoint = {
   MOVIES: 'movies',
   GENRES: 'movie_genres',
   SEANCES: 'seances',
+  USER: 'user',
 };
 
-async function getDataFromApi(endpoint) {
-  const response = await axios.get(`${api}/${endpoint}`);
-  return response;
-}
+export const defaultClient = axios.create({
+  baseURL: api,
+  headers: { 'Content-Type': 'application/json' },
+});
 
 export async function getAllMoviesData() {
-  const moviesResponse = await getDataFromApi(endpoint.MOVIES);
+  const moviesResponse = await defaultClient.get(endpoint.MOVIES);
   return moviesResponse.data;
 }
 
 export async function getMovieData(movieId) {
-  const movieResponse = await getDataFromApi(`${endpoint.MOVIES}/${movieId}`);
+  const movieResponse = await defaultClient.get(`${endpoint.MOVIES}/${movieId}`);
   return movieResponse.data;
 }
 
 export async function getMovieGenres() {
-  const genreResponse = await getDataFromApi(`${endpoint.GENRES}`);
+  const genreResponse = await defaultClient.get(`${endpoint.GENRES}`);
   return genreResponse.data;
 }
 
@@ -46,6 +47,11 @@ export async function getSeancesData(movieId = null, date = null) {
     exactEndpoint = `${endpoint.SEANCES}?movie_id=${movieId}&date=${date}`;
   }
 
-  const screeningsResponse = await getDataFromApi(exactEndpoint);
+  const screeningsResponse = await defaultClient.get(exactEndpoint);
   return screeningsResponse.data;
+}
+
+export async function getUser() {
+  const userResponse = await defaultClient.get(endpoint.USER);
+  return userResponse.data;
 }
