8850b032c8387686af96f02f0eaaa390552cc7e0
Patryk Wawrzeński <patryk.wawrzenski@monterail.com>
2022-09-07 17:51:00 +0200

    move getSecretData() test function to HeaderActions component

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,6 @@
 import { defineComponent } from 'vue';
 import { useAuthStore } from '@/store/auth.js';
 
-import { getUserData } from '@/services/api.js'; // <<< for testing only
-
 import TheHeader from '@/components/TheHeader.vue';
 
 export default defineComponent({
@@ -17,23 +15,12 @@ export default defineComponent({
   created() {
     this.auth.restoreAuth();
   },
-  methods: {
-    // for testing only >>>>
-    async getSecretData() {
-      this.user = await getUserData();
-      console.table(this.user);
-    },
-    // <<<
-  },
 });
 </script>
 
 <template>
   <div class="app">
     <TheHeader class="app__header" />
-    <!-- for testing only -->
-    <button @click="getSecretData">Get secret data</button>
-    <!---------------------->
     <router-view class="app__content" />
   </div>
 </template>
--- src/components/header/HeaderActions.vue
+++ src/components/header/HeaderActions.vue
@@ -1,16 +1,43 @@
 <script>
 import { defineComponent } from 'vue';
+import { useAuthStore } from '@/store/auth.js';
+import { getUserData } from '@/services/api.js'; // <<< for testing only
+
 import BaseButton from '@/components/base/BaseButton.vue';
 
 export default defineComponent({
   components: {
     BaseButton,
   },
+  setup() {
+    const auth = useAuthStore();
+    return { auth };
+  },
+  computed: {
+    isLoggedIn() {
+      return this.auth.isLoggedIn;
+    },
+  },
+  methods: {
+    // for testing only >>>>
+    async getSecretData() {
+      this.user = await getUserData();
+      console.table(this.user);
+    },
+    // <<<
+    logout() {
+      this.auth.logout();
+    },
+  },
 });
 </script>
 
 <template>
-  <div class="header-actions">
+  <div v-if="isLoggedIn" class="header-actions">
+    <BaseButton @click="getSecretData" button-type="hollow-red">My account</BaseButton>
+    <BaseButton @click="logout">Log out</BaseButton>
+  </div>
+  <div v-else class="header-actions">
     <BaseButton :to="{ name: 'RegisterPage' }" button-type="borderless-red">Register</BaseButton>
     <BaseButton :to="{ name: 'LoginPage' }">Login</BaseButton>
   </div>
