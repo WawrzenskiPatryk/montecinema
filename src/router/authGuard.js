f774732e1b3af42977a51e4afc6406931c75e64a
Patryk Wawrzeński <patryk.wawrzenski@monterail.com>
2022-09-14 12:08:27 +0200

    authGuard use route metadata

--- src/router/authGuard.js
+++ src/router/authGuard.js
@@ -1,22 +1,11 @@
 import { useAuthStore } from '@/store/auth.js';
-import authRoutes from './auth';
-import userRoutes from './user';
-
-const getRouteNames = routes => {
-  return routes.reduce((routeNames, route) => {
-    if (route.name) routeNames.push(route.name);
-    return routeNames;
-  }, []);
-};
 
 export const authGuard = (to, _, next) => {
   const { isLoggedIn } = useAuthStore();
-  const isAuthRoute = getRouteNames(authRoutes).includes(to.name);
-  const isUserRoute = getRouteNames(userRoutes).includes(to.name);
 
-  if (isLoggedIn && isAuthRoute) {
+  if (isLoggedIn && to.meta.requiresUnauthorized) {
     next({ name: 'UserAccountPage' });
-  } else if (!isLoggedIn && isUserRoute) {
+  } else if (!isLoggedIn && to.meta.requiresAuth) {
     next({ name: 'LoginPage', query: { redirect: to.name } });
   } else {
     next();
