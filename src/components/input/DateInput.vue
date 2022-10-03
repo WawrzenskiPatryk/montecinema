5776bfb194064d35b23b25635291423cc7346358
Patryk Wawrzeński <patryk.wawrzenski@monterail.com>
2022-10-03 14:54:31 +0200

    refactor(DateInput): rewrite to script setup with typescript

--- src/components/input/DateInput.vue
+++ src/components/input/DateInput.vue
@@ -1,41 +1,33 @@
-<script>
-import { defineComponent } from 'vue';
+<script setup lang="ts">
+import { ref, computed, onUpdated } from 'vue';
 import BaseInput from '@/components/base/BaseInput.vue';
 
-export default defineComponent({
-  components: {
-    BaseInput,
-  },
-  props: {
-    modelValue: {
-      type: [String, Number],
-      default: '',
-    },
-  },
-  updated() {
-    this.onUpdate(this.modelValue);
-  },
-  data() {
-    return {
-      isTypeDate: false,
-    };
-  },
-  computed: {
-    computedType() {
-      if (this.isTypeDate) return 'date';
-      else return 'text';
-    },
-  },
-  methods: {
-    onTouch(event) {
-      if (event.type === 'focus') this.isTypeDate = true;
-      else if (!event.target.value) this.isTypeDate = false;
-    },
-    onUpdate(value) {
-      if (value) this.isTypeDate = true;
-      this.$emit('update:modelValue', value);
-    },
-  },
+const props = defineProps<{
+  modelValue: string;
+}>();
+
+const emit = defineEmits(['update:modelValue']);
+
+const isTypeDate = ref(false);
+
+const computedType = computed(() => {
+  if (isTypeDate.value) return 'date';
+  else return 'text';
+});
+
+const onTouch = (event: Event): void => {
+  const eventTarget = event.target as HTMLInputElement;
+  if (event.type === 'focus') isTypeDate.value = true;
+  else if (!eventTarget.value) isTypeDate.value = false;
+};
+
+const onUpdate = (value: string): void => {
+  if (value) isTypeDate.value = true;
+  emit('update:modelValue', value);
+};
+
+onUpdated(() => {
+  onUpdate(props.modelValue);
 });
 </script>
 
