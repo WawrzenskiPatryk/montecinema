f49d517805a1b7abac81b3e6fbc2861f56ec75bb
Patryk Wawrzeński <patryk.wawrzenski@monterail.com>
2022-10-03 14:52:17 +0200

    feat(typescript): setup for svg and jsx: preserve

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -3,3 +3,8 @@ declare module '*.vue' {
   const Component: ReturnType<typeof defineComponent>;
   export default Component;
 }
+declare module '*.svg' {
+  import { defineComponent } from 'vue';
+  const Component: ReturnType<typeof defineComponent>;
+  export default Component;
+}
